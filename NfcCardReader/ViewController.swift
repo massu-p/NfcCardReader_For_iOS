9f8945b7887883a7567c10e569923e5da0a3c185
masukura <masukura.yutaka@opst.co.jp>
2019-06-28 17:54:21 +0900

    Initial Commit

---
+++ NfcCardReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NfcCardReader
+//
+//  Created by 増倉豊 on 2019/05/16.
+//  Copyright © 2019 jp.co.opst. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
